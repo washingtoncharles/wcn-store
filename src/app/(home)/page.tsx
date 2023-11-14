c2b8e3c1a1f8eb5e58fc75865a1f475ba345333d
Washington Charles <toncharles@live.com>
2023-11-14 20:06:41 -0300

    feat: add ProductItem and ProductList components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,26 +1,39 @@
-"use client";
-
-import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Categories from "./components/categories";
+import { prismaClient } from "@/lib/prisma";
+import ProductList from "./components/product-list";
 
-export default function Home() {
-  const {data} = useSession();
+export default async function Home() {
+  //VALIDATION FOR DISCOUNT PERCENTAGE GREATER THAN 0
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0
+      },
+    },
+    take: 10
+  });
 
   return (
-    <div className="p-5">
-      {data?.user?.name}
+    <div className="">
+      {/* HOME PAGE MAIN BANNER */}
       <Image 
-        className="w-full h-auto"
+        className="w-full h-auto px-5"
         sizes="100vw"
         src="/banner-home-01.svg"
         height={0}
         width={0}
         alt="até 55% de Desconto esse mês!"
       />
-
-      <div className="mt-8">
+      
+      {/* LIST OF CATEGORIES */}
+      <div className="mt-8 px-5">
         <Categories />
+      </div>
+
+      {/* PRODUCT LIST */}
+      <div className="mt-8">
+        <ProductList products={deals} />
       </div>
       
     </div>
