aa278bd0699d5f5970275b9654c65f8e4aeb1285
Washington Charles <toncharles@live.com>
2023-11-15 22:27:30 -0300

    feat: Category Page

---
+++ src/app/catalog/page.tsx
@@ -0,0 +1,31 @@
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { LayoutGridIcon } from "lucide-react";
+import CategoryItem from "./components/category-item";
+
+const CatalogPage = async () => {
+  const categories = await prismaClient.category.findMany({
+    
+  })
+
+  return ( 
+    <div className="flex flex-col gap-8 p-5">
+      <Badge 
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase" 
+        variant={"outline"}
+      >
+        <LayoutGridIcon size={16} />
+        Catálogo
+      </Badge>
+
+      {/* CATEGORIES */}
+      <div className="grid grid-cols-2 gap-8">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </div>
+   );
+}
+ 
+export default CatalogPage;
