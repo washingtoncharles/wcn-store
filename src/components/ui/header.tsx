a9ba09481d65dbc23e20fa05e079346c4e21dc62
Washington Charles <toncharles@live.com>
2023-11-16 19:01:19 -0300

    feat: ensure that the user accesses the home and catalog page when clicking the menu button

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -2,10 +2,11 @@
 import { HomeIcon, ListOrderedIcon, LogInIcon, LogOutIcon, MenuIcon, PercentIcon, ShoppingCart } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card"
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { Sheet, SheetClose, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { Separator } from "@radix-ui/react-separator";
+import Link from "next/link";
 
 const Header = () => {
   const {status, data} = useSession();
@@ -69,20 +70,28 @@ const Header = () => {
             )}
             
 
-            <Button variant={"outline"} className="w-full justify-start gap-2">
-              <HomeIcon size={16} />
-              Inicio
-            </Button>
+            <SheetClose asChild>
+              <Link href="/">
+                <Button variant={"outline"} className="w-full justify-start gap-2">
+                  <HomeIcon size={16} />
+                  Inicio
+                </Button>
+              </Link>
+            </SheetClose>
 
             <Button variant={"outline"} className="w-full justify-start gap-2">
               <PercentIcon size={16} />
               Ofertas
             </Button>
 
-            <Button variant={"outline"} className="w-full justify-start gap-2">
-              <ListOrderedIcon size={16} />
-              Catálogo
-            </Button>
+            <SheetClose asChild>
+              <Link href="/catalog">
+                <Button variant={"outline"} className="w-full justify-start gap-2">
+                  <ListOrderedIcon size={16} />
+                  Catálogo
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
           
         </SheetContent>
