50abdfa8758703fcb2f23fe58c188b2754b3c559
Washington Charles <toncharles@live.com>
2023-11-16 22:57:44 -0300

    feat: add ProductImages components

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { prismaClient } from "@/lib/prisma";
+import ProductImages from "./components/product-images";
 
 interface ProductDetailsPageProps {
   params: {
@@ -17,8 +18,10 @@ const ProductDetailsPage = async ({ params: { slug } }: ProductDetailsPageProps)
     return null;
   }
 
-  return ( 
-    <h1>{product.name}</h1>
+  return (
+    <div>
+      <ProductImages imageUrls={product.imageUrls} name={product.name} />
+    </div>
    );
 }
  
