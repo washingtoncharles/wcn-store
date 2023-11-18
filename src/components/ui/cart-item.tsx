dae305e9ed91affa308c5ff3339af29f157bdfda
Washington Charles <toncharles@live.com>
2023-11-18 15:54:35 -0300

    feat: add CardItem component and improved card product addition

---
+++ src/components/ui/cart-item.tsx
@@ -0,0 +1,61 @@
+import { CartProduct } from "@/providers/cart";
+import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
+import Image from "next/image";
+import { Button } from "./button";
+
+interface CartItemProps {
+  product: CartProduct;
+}
+
+const CardItem = ({product}: CartItemProps) => {
+  return ( 
+    <div className="flex items-center justify-between">
+      {/* RIGTH - IMAGE AND NAME */}
+      <div className="flex items-center gap-4">
+        <div className="bg-accent flex items-center justify-center rounded-lg h-[77px] w-[77px]">
+          <Image 
+            src={product.imageUrls[0]} 
+            alt={product.name} 
+            width={0} height={0} 
+            sizes="100vw"
+            className="h-auto w-auto max-w-[80%] max-h-[70%]"
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <p className="text-xs">{product.name}</p>
+
+          <div className="flex items-center gap-2">
+            <p className="font-bold text-sm">R$ {product.totalPrice.toFixed(2)}</p>
+            {product.discountPercentage > 0 && (
+              <p className="opacity-75 line-through text-xs">R$ {Number(product.basePrice).toFixed(2)}</p>
+            )}
+          </div>
+
+          {/* QUANTITY ITEM */}
+          <div className="flex items-center gap-1 mt-1">
+            <Button variant="outline" size="icon">
+              <ChevronLeftIcon />
+            </Button>
+
+            <span className="text-xs">{product.quantity}</span>
+
+            <Button variant="outline" size="icon">
+              <ChevronRightIcon />
+            </Button>
+          </div>
+
+        </div>
+      </div>
+
+      {/* LEFT - BUTTON DELETE */}
+      <div>
+        <Button size="icon" variant="outline">
+          <TrashIcon size={16} />
+        </Button>
+      </div>
+    </div>
+   );
+}
+ 
+export default CardItem;
--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { Product } from "@prisma/client";
+import { ProductWithTotalPrice } from "@/helpers/product";
 import { createContext, useState } from "react";
 
-interface CartProduct extends Product {
+export interface CartProduct extends ProductWithTotalPrice {
   quantity: number;
 }
 
@@ -25,6 +25,27 @@ export const CartContext = createContext<ICartContext>({
 export const CartProvider = ({children}: {children: React.ReactNode}) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
   const addProductToCart = (product: CartProduct) => {
+
+    // CHECK IF PRODUCT IS ALREADY ON CART
+    const productIsAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+
+    if(productIsAlreadyOnCart) {
+      setProducts((prev) => prev.map((cartProduct) => {
+        if(cartProduct.id === product.id) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + product.quantity,
+          };
+        }
+        return cartProduct;
+        }),
+      );
+      return;
+    }
+
+    // ADD NEW PRODUCT
     setProducts((prev) => [...prev, product]);
   };
 
