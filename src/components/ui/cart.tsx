8c922341a0824b2f48fd6622beb15b87e4e9135a
Washington Charles <toncharles@live.com>
2023-11-19 21:12:33 -0300

    feat: ensure cart products can br scrolled

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -3,14 +3,16 @@ import { Badge } from "./badge";
 import { useContext } from "react";
 import {CartContext} from "@/providers/cart";
 import CardItem from "./cart-item";
+import { ScrollArea } from "./scroll-area";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
+import { Button } from "./button";
 
 const Cart = () => {
   const {products, subTotal, totalDiscount, total} = useContext(CartContext);
 
   return ( 
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge 
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase" 
         variant={"outline"}
@@ -20,19 +22,23 @@ const Cart = () => {
       </Badge>
 
       {/* RENDER THE PRODUCTS */}
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CardItem 
-              key={product.id} 
-              product={computeProductTotalPrice(product as any) as any} 
-            />
-          ))
-        ) : (
-          <p className="text-center font-semibold">
-            Seu carrinho esta vazio!
-          </p>
-        )}
+      <div className="flex flex-col gap-5 h-full max-h-full overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-5">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CardItem 
+                  key={product.id} 
+                  product={computeProductTotalPrice(product as any) as any} 
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">
+                Seu carrinho esta vazio!
+              </p>
+            )}
+          </div>
+        </ScrollArea>
       </div>
 
       {/* CALCULATE CART TOTALS */}
@@ -61,6 +67,11 @@ const Cart = () => {
           <p>Total Geral:</p>
           <p>R$ {total.toFixed(2)}</p>
         </div>
+
+        {/* CHECKOUT */}
+        <Button className="w-full mt-5 font-semibold uppercase">
+          Finalizar Compra
+        </Button>
 
       </div>
     </div>
